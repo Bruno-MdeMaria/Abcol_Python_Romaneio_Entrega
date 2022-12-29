78bc9ba17618833ad8ad66aa87f36115fd612d01
Bruno Martins <brunomartinsdemaria@gmail.com>
2022-12-29 03:19:02 -0300

    Update main.py

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ window.title("ROMANEIO DE ENTREGA")
 window.config(padx=40, pady=40)
 window.geometry("1500x700")
 
-canvas = Canvas(height=1500, width=700)
+canvas = Canvas(height=100, width=600)
 logo_abcol = PhotoImage(file="image\logo_abcol.png")
 canvas.create_image(600,100, image=logo_abcol)
 canvas.grid(row=0, column=1)
