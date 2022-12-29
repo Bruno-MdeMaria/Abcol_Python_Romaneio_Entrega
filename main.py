c268fd4e73270b1f161891da2b78dbb02f9344db
Bruno Martins <brunomartinsdemaria@gmail.com>
2022-12-29 02:57:13 -0300

    divs
    criado imagem logo

--- main.py
+++ main.py
@@ -7,7 +7,12 @@ import json
 window = Tk()
 window.title("ROMANEIO DE ENTREGA")
 window.config(padx=40, pady=40)
-window.geometry("1500x700") 
+window.geometry("1500x700")
+
+canvas = Canvas(height=1500, width=700)
+logo_abcol = PhotoImage(file="image\logo_abcol.png")
+canvas.create_image(600,100, image=logo_abcol)
+canvas.grid(row=0, column=1)
 
 
 
