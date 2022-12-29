67ae6759f52c9b458ab813b3446e978d6b19611d
Bruno Martins <brunomartinsdemaria@gmail.com>
2022-12-29 02:40:32 -0300

    Create main.py
    criado janela

---
+++ main.py
@@ -0,0 +1,14 @@
+from tkinter import *
+from tkinter import messagebox
+import pyperclip
+import json
+
+# ---------------------------- UI SETUP ------------------------------- #
+window = Tk()
+window.title("ROMANEIO DE ENTREGA")
+window.config(padx=40, pady=40)
+window.geometry("1500x700") 
+
+
+
+window.mainloop()
